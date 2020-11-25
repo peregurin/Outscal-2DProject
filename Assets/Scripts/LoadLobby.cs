43352f4cae73e7dfa236dc92ad20c6bb7b53120d
peregurin <chinmayvirmani@gmail.com>
2020-11-25 16:03:35 +0530

    Added and refined Lobby UI and LobbyController Script

---
+++ Assets/Scripts/LoadLobby.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace LoadLobby
+{
+    public class LoadLobby : MonoBehaviour
+    {
+        [SerializeField]
+        Button goBackButton;
+
+        void Start()
+        {
+            goBackButton.onClick.AddListener(LoadMasterScene);
+        }
+
+        private void LoadMasterScene()
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/Scripts/LobbyController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace LobbyController
+{
+    public class LobbyController : MonoBehaviour
+    {
+        [SerializeField]
+        Button buttonPlay, buttonLevel1, buttonLevel2, buttonLevel3;
+
+        [SerializeField]
+        GameObject levelSelectionPopup;
+
+        private void Start()
+        {
+            buttonPlay.onClick.AddListener(LoadPopup);
+            buttonLevel1.onClick.AddListener(() => LoadLevel(1));
+            buttonLevel2.onClick.AddListener(() => LoadLevel(2));
+            buttonLevel3.onClick.AddListener(() => LoadLevel(3));
+        }
+
+        private void LoadPopup()
+        {
+            buttonPlay.gameObject.SetActive(false);
+            levelSelectionPopup.SetActive(true);
+        }
+
+        public void LoadLevel(int sceneBuildIndex)
+        {
+            SceneManager.LoadScene(sceneBuildIndex);
+        }
+    }
+}
